40a4227a0a60a22d5801911050502c8a59fb38d6
Sonke Wissel <wissels@hotmail.com>
2018-08-29 17:20:19 +0200

    Update

--- src/io/estrado/Pipeline.groovy
+++ src/io/estrado/Pipeline.groovy
@@ -94,7 +94,8 @@ def containerBuildPub(Map args) {
 
     println "Running Docker build/publish: ${args.host}/${args.acct}/${args.repo}:${args.tags}"
 
-    docker.withRegistry("https://${args.host}", "${args.auth_id}") {
+    //docker.withRegistry("https://${args.host}", "${args.auth_id}") {
+    docker.withRegistry("", "${args.auth_id}") {
 
         // def img = docker.build("${args.acct}/${args.repo}", args.dockerfile)
         def img = docker.image("${args.acct}/${args.repo}")
